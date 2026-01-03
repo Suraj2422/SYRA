3381c2e18a2260b3613cf7973a3791b72fcd5188
Suraj2422 <suraj.ghodke_24uds@sanjivani.edu.in>
2026-01-03 13:03:37 +0530

    Initial project structure and assistant skeleton

---
+++ core/assistant.py
@@ -0,0 +1,6 @@
+class Assistant:
+    def __init__(self):
+        print("[INIT] SYRA initialized")
+
+    def start(self):
+        print("[START] SYRA is online and waiting for commands...")
---
+++ main.py
@@ -0,0 +1,5 @@
+from core.assistant import Assistant
+
+if __name__ == "__main__":
+    jarvis = Assistant()
+    jarvis.start()
